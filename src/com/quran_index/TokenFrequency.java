125796b05c2ccfc0f1be7d037c97fa7e33e0acd4
Hashim Mir <hashim.mir123@gmail.com>
2018-06-02 17:34:56 -0700

    ssample breakdown of Surah Nas

--- src/com/quran_index/TokenFrequency.java
+++ src/com/quran_index/TokenFrequency.java
@@ -2,17 +2,35 @@ package com.quran_index;
 
 import org.jqurantree.analysis.AnalysisTable;
 import org.jqurantree.analysis.SortOrder;
+import org.jqurantree.orthography.Chapter;
 import org.jqurantree.orthography.Document;
 import org.jqurantree.orthography.Token;
+import org.jqurantree.orthography.Verse;
+
+import java.util.Iterator;
 
 public class TokenFrequency {
 
     public static void main(String[] args) {
-        // Example #1.
-        topTokensWithDiacritics();
+        topTokensBySurah(114);
+    }
 
-        // Example #2.
-        topTokensWithoutDiacritics();
+    private static void topTokensBySurah(int chapterNumber) {
+        AnalysisTable table = new AnalysisTable("Token");
+
+        Chapter chapter = Document.getChapter(chapterNumber);
+        for (Iterator<Verse> it = chapter.iterator(); it.hasNext();) {
+            Verse verse = it.next();
+            for (Token token : verse.getTokens()) {
+                System.out.println(token.removeDiacritics().toUnicode());
+                table.add(token.removeDiacritics().toUnicode());
+            }
+        }
+
+        // Group and display top 10 results.
+        AnalysisTable groupTable = table.group("Token");
+        groupTable.sort("Count", SortOrder.Descending);
+        System.out.println(groupTable.toString());
     }
 
     private static void topTokensWithDiacritics() {
